1508f3583dd7ccc996bf144409be36e24302881b
calebisholaa <isholaa@etsu.edu>
2022-09-22 16:04:26 -0400

    Added Open and Read function

---
+++ MemoryHierarchySimulator/OpenFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryHierarchySimulator
+{
+    internal class OpenFile
+    {
+        public void OpenReadFile()
+        {
+            using(OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+
+            }
+        }
+    }
+}
