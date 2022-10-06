9e5192adec6d96a2cfae4ec4fed382f0d01c2c72
calebisholaa <isholaa@etsu.edu>
2022-10-06 09:34:19 -0400

    Commit

--- MemoryHierarchySimulator/OpenFile.cs
+++ MemoryHierarchySimulator/OpenFile.cs
@@ -11,37 +11,13 @@ namespace MemoryHierarchySimulator
     internal class OpenFile
     {
 
-        IDictionary<string, string> trace = new Dictionary<string, string>();
+        /// IDictionary<string, string> trace = new Dictionary<string, string>();
+        /// List<KeyValuePair<string, string>> list = new List<KeyValuePair<string, string>>();
         List<string> accessType = new List<string>();
         List<string> hexAddress = new List<string>();
         string filePath = "";
         string fileContent = "";
-        public void OpenReadFile()
-        {
-            using(OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                openFileDialog.InitialDirectory = "c:\\";
-                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-                openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = true;
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    //Get the path of specified file
-                    this.filePath = openFileDialog.FileName;
-
-                    //Read the contents of the file into a stream
-                    var fileStream = openFileDialog.OpenFile();
-
-                    using (StreamReader reader = new StreamReader(fileStream))
-                    {
-                        fileContent = reader.ReadToEnd();
-                    }
-                }
-
-            }
-        }
-
+       
         public void OpenAndParse()
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
@@ -79,9 +55,8 @@ namespace MemoryHierarchySimulator
                                 accessType.Add(values[0]);
                                 hexAddress.Add(values[1]);
 
-                               // trace.Add(values[0], values[1]);
+                               // trace.Add(values[0], values[1]);  dictionaries would not allow duplicate keys 
                                 
-
                                 lineCounter++;
                             }
 
