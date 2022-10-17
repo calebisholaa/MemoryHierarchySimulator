097838c9c78dd31cfb95a2e3717c96a9b354c790
TimeTravelingSoldier <85300161+TimeTravelingSoldier@users.noreply.github.com>
2022-10-17 15:36:45 -0400

    Update Page.cs
    Add on two methods for the program to use. Haven't had the chance to test them yet, but they should give the contents of the correct table, and give you whether or not that table entry would have the data entry you are looking for. The main method is the same. Whoever is going to make the code to traverse the hierarchy, tell me if you have any problems with the code. Should have getPhysicalAddress() return the full physical address as the end result in the coming days, with offset added to it.

--- MemoryHierarchySimulator/Page.cs
+++ MemoryHierarchySimulator/Page.cs
@@ -1,7 +1,7 @@
 /*
     Developer: Joshua Thomas
     Start Date: 10/12/22
-    Last Revised: 10/12/22
+    Last Revised: 10/17/22
     Description: Page table object type and traversal
 */
 
@@ -22,11 +22,28 @@ namespace MemoryHierarchySimulator
             int maxSize = ms;//size of Frames and Pages
             int numFrames = physicalSize / maxSize;
             int numPages = virtualSize / maxSize;
-            int pageNumber = Math.Log2(numPages);
+            int pageNumber = Math.Log2(numPages);//amount of bits used for page number
             int pageOffset = Math.Log2(virtualSize) - pageNumber;
-            int frameNumber = Math.Log2(numFrames);
+            int frameNumber = Math.Log2(numFrames);//amount of bits used for frame number
             int frameOffest = Math.Log2(physicalSize) - frameNumber;
-            int[,] table = new int[pageNumber,frameNumber];//initial version of what page table would be structured. Dosn't support offset yet. Maybe use two more layers for offset?
+            bool[] presentTable = new bool[pageNumber];//holds weather a specific page holds a value currently
+            string[] addressTable = new string[pageNumber];//holds the physical address the page holds
+        }//potential two arrays, with virtual page number as index, first with weather its durty, and the other is the physical frame number
+
+        static string getPhysicalAddress(string address)
+        {
+            string bitAddress = Convert.ToString(Convert.ToInt64(bitAddress, 16), 2);//convert hes address to individual bits
+            bitAddress = bitAddress >> pageOffset;//remove all but the bits that identify where in the page table the physical address is.
+            int index = Convert.ToInt32(bitAddress,2);
+            return addressTable[index];//returns address to be used in physical memory. Need offset added.
+        }
+
+        static bool getPresentBit(string address)
+        {
+            string bitAddress = Convert.ToString(Convert.ToInt64(bitAddress, 16), 2);//convert hes address to individual bits
+            bitAddress = bitAddress >> pageOffset;//remove all but the bits that identify where in the page table the physical address is.
+            int index = Convert.ToInt32(bitAddress,2);
+            return presentTable[index];//returns whether or not the address would have anything to grab.
         }
     }
 }
