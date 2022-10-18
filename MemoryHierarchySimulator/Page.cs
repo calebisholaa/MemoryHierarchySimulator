a1c383360572da5fb406a098a4025345af8fd33a
TimeTravelingSoldier <85300161+TimeTravelingSoldier@users.noreply.github.com>
2022-10-17 20:37:50 -0400

    Finish getPhysicalAddress
    Made the method getPhysicalAddress output the hexadecimal address for the physical memory, which includes the offset. Also made the variables declared in what is now the PageTable constructor available for the entire class, not just the constructor. Not tested yet, but is error-free and theoretically sound. Any problems with the code can be directed to me.

--- MemoryHierarchySimulator/Page.cs
+++ MemoryHierarchySimulator/Page.cs
@@ -15,32 +15,45 @@ namespace MemoryHierarchySimulator
 {
     public class PageTable
     {
-        static void main(int vs, int ps, int ms)//making of page tables, vs is virtual size, ps is physical size, ms is max size of virtual entries
+        int virtualSize;
+        int physicalSize;
+        int maxSize;
+        int numFrames;
+        int numPages;
+        int pageNumber;
+        int pageOffset;
+        int frameNumber;
+        int frameOffest;
+        bool[] presentTable;
+        string[] addressTable;
+        public PageTable(int vs, int ps, int ms)//making of page tables, vs is virtual size, ps is physical size, ms is max size of virtual entries
         {
-            int virtualSize = vs;
-            int physicalSize = ps;
-            int maxSize = ms;//size of Frames and Pages
-            int numFrames = physicalSize / maxSize;
-            int numPages = virtualSize / maxSize;
-            int pageNumber = Math.Log2(numPages);//amount of bits used for page number
-            int pageOffset = Math.Log2(virtualSize) - pageNumber;
-            int frameNumber = Math.Log2(numFrames);//amount of bits used for frame number
-            int frameOffest = Math.Log2(physicalSize) - frameNumber;
-            bool[] presentTable = new bool[pageNumber];//holds weather a specific page holds a value currently
-            string[] addressTable = new string[pageNumber];//holds the physical address the page holds
-        }//potential two arrays, with virtual page number as index, first with weather its durty, and the other is the physical frame number
+            virtualSize = vs;
+            physicalSize = ps;
+            maxSize = ms;//size of Frames and Pages
+            numFrames = physicalSize / maxSize;
+            numPages = virtualSize / maxSize;
+            pageNumber = Math.Log2(numPages);//amount of bits used for page number
+            pageOffset = Math.Log2(virtualSize) - pageNumber;
+            frameNumber = Math.Log2(numFrames);//amount of bits used for frame number
+            frameOffest = Math.Log2(physicalSize) - frameNumber;
+            presentTable = new bool[pageNumber];//holds weather a specific page holds a value currently
+            addressTable = new string[pageNumber];//holds the physical address the page holds
+        }
 
         static string getPhysicalAddress(string address)
         {
-            string bitAddress = Convert.ToString(Convert.ToInt64(bitAddress, 16), 2);//convert hes address to individual bits
-            bitAddress = bitAddress >> pageOffset;//remove all but the bits that identify where in the page table the physical address is.
-            int index = Convert.ToInt32(bitAddress,2);
-            return addressTable[index];//returns address to be used in physical memory. Need offset added.
+            string bitAddress = Convert.ToString(Convert.ToInt64(address, 16), 2);//convert the address to individual bits
+            string locAddress = bitAddress >> pageOffset;//remove all but the bits that identify where in the page table the physical address is.
+            int index = Convert.ToInt32(locAddress,2);
+            string phyAddress = addressTable[index];
+            string offAddress = (bitAddress << pageNumber) >> pageNumber;//zero out parts of the string that represent the virtual address
+            return Convert.ToString(Convert.ToInt64((phyAddress | offAddress), 2), 16);//combines the phyical address and the offset into one hexedecimal address
         }
 
         static bool getPresentBit(string address)
         {
-            string bitAddress = Convert.ToString(Convert.ToInt64(bitAddress, 16), 2);//convert hes address to individual bits
+            string bitAddress = Convert.ToString(Convert.ToInt64(address, 16), 2);//convert hes address to individual bits
             bitAddress = bitAddress >> pageOffset;//remove all but the bits that identify where in the page table the physical address is.
             int index = Convert.ToInt32(bitAddress,2);
             return presentTable[index];//returns whether or not the address would have anything to grab.
