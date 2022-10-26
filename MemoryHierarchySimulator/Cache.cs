b86040b6c9d2fdd8da8e10425e8a3cb35453d1bb
Caleb Ishola <ISHOLAA@ETSU.EDU>
2022-10-26 17:39:43 -0400

    Setting up block/line, sets and cache

--- MemoryHierarchySimulator/Cache.cs
+++ MemoryHierarchySimulator/Cache.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Globalization;
 using System.Linq;
 using System.Security.Cryptography;
@@ -16,7 +17,7 @@ namespace MemoryHierarchySimulator
 
         public  readonly OpenConfigFile openConfigFile;
 
-        public List<int[]> sets = new List<int[]>();
+        public List<List<int[]>> sets = new List<List<int[]>>();   //i don't know which might be easier, int[] of string 
 
         //public OpenConfigFile OpenConfigFile => openConfigFile;
 
@@ -36,32 +37,50 @@ namespace MemoryHierarchySimulator
 
         public void CreateCaches()
         {
-            
+            Console.WriteLine("Creating Caches");
             var numberOfSet = openConfigFile.NumberofSets;
-            var setSize = openConfigFile.SetSize;
+            var setSize = openConfigFile.SetSize;   //since i am using lists this might be redundant 
+            var lineSize = openConfigFile.LineSize;
+            var offSet = openConfigFile.OffSetBits;
+            var addressSize = GetAddressSize(openConfigFile.PhysicalPages, openConfigFile.PageSize);
+           
             
             while(numberOfSet > 0)
             {
-                sets.Add(MakeArray(setSize));
+                sets.Add(CreateSets(CreateBlock(lineSize), setSize));
+                
                 numberOfSet--;
             }
 
-            Console.WriteLine("Number of Set -> " + sets.Count);
-            foreach (var item in sets)
-            {
-                Console.WriteLine(item.ToString());
-            }
+            Console.WriteLine("Number of Set: " + sets.Count);
+            Console.WriteLine("");
+            
 
         }
 
-        public int[] MakeArray(int size)
+        public int[] CreateBlock(int lineSize)
         {
-            int[] newArray = new int[size];
-
+            int[] newArray = new int[lineSize];
             return newArray;
         }
 
+        public List<int[]> CreateSets(int[] block, int setSize)
+        {
+            List<int[]> newList = new List<int[]>(setSize);
+            newList.Add(block);
+            return newList;
+        }
 
+        /// <summary>
+        /// Returns the Address Size. This is derived by multiplying the physical page by the page size
+        /// </summary>
+        /// <param name="physicalPages"></param>
+        /// <param name="pageSize"></param>
+        /// <returns>addres size in bytes</returns>
+        public int GetAddressSize(int physicalPages,int pageSize)
+        {
+            return physicalPages * pageSize;   
+        }
        
     }
 }
