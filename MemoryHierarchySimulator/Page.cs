61d2ed5dbddad1ade745482ca2344b799bbc65ba
TimeTravelingSoldier <85300161+TimeTravelingSoldier@users.noreply.github.com>
2022-10-31 12:30:53 -0400

    Update Page.cs
    Made code compilable. Still can't test the code on my end. All my debuggers are broken, and I haven't been able to fix them in months. Can someone test my code to see what it outputs?

--- MemoryHierarchySimulator/Page.cs
+++ MemoryHierarchySimulator/Page.cs
@@ -1,7 +1,7 @@
 /*
     Developer: Joshua Thomas
     Start Date: 10/12/22
-    Last Revised: 10/17/22
+    Last Revised: 10/31/22
     Description: Page table object type and traversal
 */
 
@@ -26,17 +26,17 @@ namespace MemoryHierarchySimulator
         int frameOffest;
         bool[] presentTable;
         string[] addressTable;
-        public PageTable(int vs, int ps, int ms)//making of page tables, vs is virtual size, ps is physical size, ms is max size of virtual entries
+        public PageTable(int vp, int pp, int ms)//making of page tables, vp is amount of virtual pages, pp is amount of physical pages, ms is max size of each page
         {
-            virtualSize = vs;
-            physicalSize = ps;
+            numPages = vp;
+            numFrames = pp;
             maxSize = ms;//size of Frames and Pages
-            numFrames = physicalSize / maxSize;
-            numPages = virtualSize / maxSize;
-            pageNumber = Math.Log2(numPages);//amount of bits used for page number
-            pageOffset = Math.Log2(virtualSize) - pageNumber;
-            frameNumber = Math.Log2(numFrames);//amount of bits used for frame number
-            frameOffest = Math.Log2(physicalSize) - frameNumber;
+            virtualSize = maxSize * numPages;
+			physicalSize = numFrames * maxSize;
+			pageNumber = (int)Math.Log2(numPages);//amount of bits used for page number
+            pageOffset = (int)Math.Log2(virtualSize) - pageNumber;
+            frameNumber = (int)Math.Log2(numFrames);//amount of bits used for frame number
+            frameOffest = (int)Math.Log2(physicalSize) - frameNumber;
             presentTable = new bool[pageNumber];//holds weather a specific page holds a value currently
             addressTable = new string[pageNumber];//holds the physical address the page holds
         }
@@ -44,17 +44,17 @@ namespace MemoryHierarchySimulator
         static string getPhysicalAddress(string address)
         {
             string bitAddress = Convert.ToString(Convert.ToInt64(address, 16), 2);//convert the address to individual bits
-            string locAddress = bitAddress >> pageOffset;//remove all but the bits that identify where in the page table the physical address is.
-            int index = Convert.ToInt32(locAddress,2);
-            string phyAddress = addressTable[index];
-            string offAddress = (bitAddress << pageNumber) >> pageNumber;//zero out parts of the string that represent the virtual address
-            return Convert.ToString(Convert.ToInt64((phyAddress | offAddress), 2), 16);//combines the phyical address and the offset into one hexedecimal address
-        }
+			string locAddress = bitAddress.Remove(pageOffset);//remove all but the bits that identify where in the page table the physical address is.
+			int index = Convert.ToInt32(locAddress, 2);
+			string phyAddress = addressTable[index];
+			string offAddress = bitAddress.Substring(0,pageOffset);// << pageNumber) >> pageNumber;//zero out parts of the string that represent the virtual address
+			return Convert.ToString(Convert.ToInt64((phyAddress + offAddress), 2), 16);//combines the phyical address and the offset into one hexedecimal address
+		}
 
         static bool getPresentBit(string address)
         {
             string bitAddress = Convert.ToString(Convert.ToInt64(address, 16), 2);//convert hes address to individual bits
-            bitAddress = bitAddress >> pageOffset;//remove all but the bits that identify where in the page table the physical address is.
+            bitAddress = bitAddress.Remove(pageOffset);//remove all but the bits that identify where in the page table the physical address is.
             int index = Convert.ToInt32(bitAddress,2);
             return presentTable[index];//returns whether or not the address would have anything to grab.
         }
