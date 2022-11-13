fe7fd70fb05298ca3ad1a237d576cc197c73c409
ZergRushNoooo <92892716+ZergRushNoooo@users.noreply.github.com>
2022-11-12 23:39:23 -0500

    Memory Hierarchy Started
    Began working on the HierarchyTransversal using Kyle's code will be communicating with him.

---
+++ MemoryHierarchySimulator/HierarchyTransversal.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------------------
+// File name: HierarchyTransversal
+// Project name: Memory Hierarchy Simulator
+// ---------------------------------------------------------------------------
+// Creator’s name and email: Stephen Maurer Maurers@etsu.edu
+// Creation Date: 11/12/2022
+// ---------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryHierarchySimulator
+{
+    class HierarchyTransversal
+    {
+
+    }
+}
