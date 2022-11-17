59a2de790f14df8441bf50d6be68108ed1810ecf
Tyseiber10 <65461024+Tyseiber10@users.noreply.github.com>
2022-11-16 23:28:15 -0500

    Updated DTLB

--- DTLB.cs
+++ DTLB.cs
@@ -4,160 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MemoryHierarchy
+namespace MemH
 {
     public class DTLB
     {
-        OpenFile openFile = new OpenFile();
-        public static OpenTrace openTrace = new OpenTrace();
-        double addressSpace = 0;
-        string VPN = " ";
-        string[] TLB = new string[openTrace.TLBSize];               //DTLB
-
-        //create DTLB and check with the VPN if you have the translation if not send it to the page table if you do then translate. 
-        //need to figure out the size of DTLB. create it dynamically not staticly. 
-
+        string[,] TLB = new string[64,1];
+        int TLBSize = 0;
 
         /// <summary>
-        /// Gets the virtual page number from the page. for now it is hard coded for the first trace file 
+        /// Searches the DTLB for a tag. If the DTLB has the tag then it will return the Physical Page Number (PPN).
+        /// If the DTLB does not have the tag then it will copy the tag within the DTLB.
         /// </summary>
-        /// <param name="virtualAddressses"></param>
-        /// <returns></returns>
-        public double GetVPN(string[,] virtualAddressses)
+        /// <param name="tag"></param>
+        /// <returns>Returns the PPN. If the PPN is not found then the method will return 'false'.</returns>
+        public string SearchTLB(string tag)
         {
-            for(int i = 0; i < virtualAddressses.GetLength(0); i++)
+            bool hit = false;
+            string PPN = " ";
+            for(int i = 0; i < TLB.GetLength(0); i++)
             {
-                for(int j = 1; j < 2; j++)
+                if (TLB[i,0] == tag)
                 {
-                    VPN = Translate(virtualAddressses[i, j]);
-                    Console.WriteLine(VPN);
+                    hit = true;
+                    PPN = TLB[i,1];
+                    return PPN;
                 }
             }
 
+            if(TLBSize > 64)
+            {
+                TLBSize = 0;
+                TLB[TLBSize, 0] = tag;
+                TLB[TLBSize,1] = " ";
+                TLBSize++;
+            }
+            else
+            {
+                TLB[TLBSize, 0] = tag;
+                TLBSize++;
+            }
 
-            OpenFile of = new OpenFile();
-            addressSpace = Math.Log2(of.pageSizeTLB);
-            
-
-            return 0;
+            PPN = "false";
+            return PPN;
         }
 
-
         /// <summary>
-        /// wrote this method to convert 2D array
+        /// Method will set the PPN for a specific tag within the DTLB
         /// </summary>
-        /// <param name="stringarray"></param>
-        /// <returns></returns>
-        public string[] Convert2D(string[,] stringarray)
+        /// <param name="tag"></param>
+        /// <param name="PPN"></param>
+        public void SetPPN(string tag, string PPN)
         {
-            string[] convertedArray = new string[stringarray.GetLength(1)];
-            int temp = 0;
-            for (int i = 0; i < stringarray.GetLength(0); i++)
+            for(int i = 0; i < TLB.GetLength(0); i++)
             {
-                for (int j = 0; j < stringarray.GetLength(1); j++)
+                if (TLB[i,0] == tag)
                 {
-                    if(stringarray[i,j].Equals("R"))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        convertedArray[temp] += stringarray[i,j];
-                    }
+                    TLB[i,1] = PPN;
                 }
-
-                temp++;
             }
-
-
-            return convertedArray;
-        }
-
-
-
-
-
-        /// <summary>
-        /// Gets the physical address
-        /// </summary>
-        /// <param name="address"></param>
-        /// <returns></returns>
-        public string GetPhysicalAddress(string address)
-        {
-            OpenFile of = new OpenFile();
-            OpenTrace ot = new OpenTrace();         
-            string bitAddress = "00" + Convert.ToString(Convert.ToInt64(address, 16), 2);//convert the address to individual bits, aditional 0's to make bits to 14 bit length needed
-            string locAddress = bitAddress.Remove((int)of.pageOffsetTLB);//remove all but the bits that identify where in the page table the physical address is.
-            Console.WriteLine(Convert.ToString(Convert.ToInt64(bitAddress, 2), 16));
-            int index = Convert.ToInt32(locAddress, 2);
-            string phyAddress = ot.address[index];
-            string offAddress = bitAddress.Substring(0, (int)of.pageOffsetTLB);//zero out parts of the string that represent the virtual address
-            return Convert.ToString(Convert.ToInt64((phyAddress + offAddress), 2), 16);//combines the phyical address and the offset into one hexedecimal address
-        }
-
-
-
-        /// <summary>
-        /// Translate the hex to binary
-        /// </summary>
-        /// <param name="s"></param>
-        /// <returns></returns>
-        public string Translate(string s)
-        {
-            string bits = " ";
-            switch(s)
-            {
-                case "1":
-                    bits = "0001";
-                    break;
-                case "2":
-                    bits = "0010";
-                    break;
-                case "3":
-                    bits = "0011";
-                    break;
-                case "4":
-                    bits = "0100";
-                    break;
-                case "5":
-                    bits = "0101";
-                    break;
-                case "6":
-                    bits = "0110";
-                    break;
-                case "7":
-                    bits = "0111";
-                    break;
-                case "8":
-                    bits = "1000";
-                    break;
-                case "9":
-                    bits = "1001";
-                    break;
-                case "a":
-                    bits = "1010";
-                    break;
-                case "b":
-                    bits = "1011";
-                    break;
-                case "c":
-                    bits = "1100";
-                    break;
-                case "d":
-                    bits = "1101";
-                    break;
-                case "e":
-                    bits = "1110";
-                    break;
-                case "f":
-                    bits = "1111";
-                    break;
-                default:
-                    break; 
-            }
-
-            return bits;
-
         }
 
     }
